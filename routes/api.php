52604e88ae049cb36ba89e5eb2fb6ae02aca4060
Windelle John Vega <windellevega@gmail.com>
2017-12-11 16:33:16 +0800

    Added comments on api routes for more readability

--- routes/api.php
+++ routes/api.php
@@ -3,61 +3,93 @@
 use Illuminate\Http\Request;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+--------------------------
+ROUTES FOR PRODUCT METHODS
+--------------------------
 */
 
+//Show list of products.
+//Optional: id - to filter products per user
 Route::middleware('auth:api')->get('/products/{id?}', [
 	'as' => 'products-list', 
 	'uses' => 'FarmerProductController@index'
 ]);
 
+//Show product detail
+//Required: id - product id
 Route::middleware('auth:api')->get('/product/{id}', [
 	'as' => 'product-show',
 	'uses' => 'FarmerProductController@show'
 ]);
 
+//Add product
 Route::middleware('auth:api')->post('/product/add', [
 	'as' => 'product-add',
 	'uses' => 'FarmerProductController@store'
 ]);
 
+
+/*
+-----------------------
+ROUTES FOR USER METHODS
+-----------------------
+*/
+
+//Show list of users
+//Optional: type - to filter users per type (farmer or user)
 Route::middleware('auth:api')->get('/users/{type?}', [
 	'as' => 'users-list',
 	'uses' => 'UserController@index'
 ]);
 
+//Show details of a user
+//Optional: id - returns detail specific user otherwise the logged-in user
 Route::middleware('auth:api')->get('/user/{id?}', [
 	'as' => 'user-show',
 	'uses' => 'UserController@show'
 ]);
 
+
+/* 
+------------------------
+ROUTES FOR ORDER METHODS
+------------------------
+*/
+
+//Initializes a new order
 Route::middleware('auth:api')->post('/order/new', [
 	'as' => 'order-new',
 	'uses' => 'OrderController@store'
 ]);
 
+//Retrieves the list of orders per user
 Route::middleware('auth:api')->get('/orders', [
 	'as' => 'orders-list', 
 	'uses' => 'OrderController@index'
 ]);
 
+//Shows details of a specific order
+//Required: id - order id
 Route::middleware('auth:api')->get('/order/{id}', [
 	'as' => 'order-show',
 	'uses' => 'OrderController@show'
 ]);
 
+
+/*
+-----------------------
+ROUTES FOR CART METHODS
+-----------------------
+*/
+
+//Add specific product to cart
 Route::middleware('auth:api')->post('/cart/add', [
 	'as' => 'cart-add',
 	'uses' => 'ProductOrderController@store'
 ]);
 
+
+//Test for uploading image
 Route::middleware('auth:api')->post('/product/add-image', [
 	'as' => 'product-add-image',
 	'uses' => 'FarmerProductController@update'
