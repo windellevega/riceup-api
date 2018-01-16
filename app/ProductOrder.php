4f9282ccb87bf840ba368025eee9757e4e490658
Windelle John Vega <windellevega@gmail.com>
2018-01-16 16:40:47 +0800

    Included deleted products on order details

--- app/ProductOrder.php
+++ app/ProductOrder.php
@@ -28,6 +28,6 @@ class ProductOrder extends Model
     **/
     public function FarmerProduct()
     {
-        return $this->belongsTo('App\FarmerProduct', 'fp_id');
+        return $this->belongsTo('App\FarmerProduct', 'fp_id')->withTrashed();
     }
 }
