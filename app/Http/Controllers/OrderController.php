688d0a892b17bc88024a052b39c2ebf8a17a459d
Windelle <windellevega@gmail.com>
2018-11-22 00:01:05 +0800

    Update on show orders added farmerproduct details

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -89,8 +89,7 @@ class OrderController extends Controller
         $order = Order::where('id', $id)
                     ->where('user_id', Auth::id())
                     ->first();
-        $order->load('ProductOrder.FarmerProduct.User');
-        $order->load('ProductOrder.currentStatus');
+        $order->load('ProductOrder.currentStatus.FarmerProduct.User');
 
         if($order->count() != 0) {
             return response()->json($order);
