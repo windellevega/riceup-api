c148a5b6890023245791bf43ad1afa5443102d50
Windelle John Vega <windellevega@gmail.com>
2019-08-01 22:13:23 +0800

    Configured laravel-pusher

---
+++ database/migrations/2019_08_01_132151_create_conversations_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConversationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_one_id')
+                ->unsigned();
+            $table->foreign('user_one_id')
+                ->references('id')
+                ->on('users');
+            $table->integer('user_two_id')
+                ->unsigned();
+            $table->foreign('user_two_id')
+                ->references('id')
+                ->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversations');
+    }
+}
